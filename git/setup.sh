29b57e5f247450fa5419aa8fa4ec0e3876792620
keatmin <keatmin3@gmail.com>
2023-05-13 22:38:00 +1000

    [ktmn] build(global gitignore): add global gitignore

--- git/setup.sh
+++ git/setup.sh
@@ -5,6 +5,8 @@ git config --global gpg.format ssh
 # Push to same name
 # git config --global commit.gpgsign true
 git config --global init.defaultbranch main
-git config --global commit.template `${pwd}/git/.gitmessage.txt`
+git config --global commit.template "$(pwd)/git/.gitmessage.txt"
+git config --global core.excludesFile "$(pwd)/git/.gitignore_global"
 
 git config --global push.default current
+
